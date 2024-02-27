d744a93492cc8150d0e9f2317928efa22d106a90
Mth0158 <mathieu.eustachy@gmail.com>
2024-02-27 10:31:27 +0100

    Super feature pour anymer l'app

---
+++ app/javascript/controllers/toggle_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="toggle"
+export default class extends Controller {
+  static targets = ["form"]
+
+  connect() {
+  }
+
+  fire(event) {
+    event.preventDefault()
+
+    this.formTarget.classList.toggle("d-none")
+  }
+}
